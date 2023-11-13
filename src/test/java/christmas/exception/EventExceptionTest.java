8bb08a230291d82ff21d23f99c3cf498e2d53b6a
jiu6525 <jiu6525@daum.net>
2023-11-13 20:00:02 +0900

    test : 사용자 날짜입력 검증 테스트

---
+++ src/test/java/christmas/exception/EventExceptionTest.java
@@ -0,0 +1,25 @@
+package christmas.exception;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.Application;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventExceptionTest extends NsTest {
+    @Test
+    @DisplayName("비정상적인 날짜 입력시 예외 테스트")
+    void VisitDateRangeCheckTest() {
+        assertSimpleTest(() -> {
+            runException("32");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
