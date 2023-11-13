c1058b2346eb8ec481446e689d7982a2a7b96894
jiu6525 <jiu6525@daum.net>
2023-11-13 20:07:56 +0900

    feat : Order 일급 컬렉션 추가

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Order {
+
+    private final LinkedHashMap<String, Integer> userOrders;
+
+    public Order(String[] orders) {
+        this.userOrders = new LinkedHashMap<>();
+        addUserOrders(orders);
+    }
+
+    public void addItem(String itemName, int quantity) {
+        userOrders.merge(itemName, quantity, Integer::sum);
+    }
+
+    public Map<String, Integer> getUserOrders() {
+        return new LinkedHashMap<>(userOrders);
+    }
+
+    public void addUserOrders(String[] orders) {
+        Arrays.stream(orders)
+                .map(order -> order.trim().split("-"))
+                .forEach(menuAndQuantity -> addItem(menuAndQuantity[0], Integer.parseInt(menuAndQuantity[1])));
+    }
+
+}
