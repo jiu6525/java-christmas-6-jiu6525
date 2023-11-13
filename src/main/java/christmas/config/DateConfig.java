1a462e60dc45e124fd68688c726d7285a6f013cb
jiu6525 <jiu6525@daum.net>
2023-11-13 19:45:10 +0900

    feat : 날짜 환경설정 클래스 추가

---
+++ src/main/java/christmas/config/DateConfig.java
@@ -0,0 +1,11 @@
+package christmas.config;
+
+public class DateConfig {
+    public static final int WEEK = 7;
+    public static final int SPECIAL_DISCOUNT_DATE = 3;
+    public static final int WEEKLY_DISCOUNT_START_DATE = 1;
+    public static final int WEEKLY_DISCOUNT_END_DATE = 2;
+    public static final int MAIN_EVENT_DATE = 25;
+    public static final int MONTH_START = 1;
+    public static final int MONTH_END = 31;
+}
