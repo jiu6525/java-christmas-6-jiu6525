6a9407417f9e635942346248688d18def21f4ecc
jiu6525 <jiu6525@daum.net>
2023-11-13 19:52:45 +0900

    feat : 방문날짜 범위 확인 메서드 추가

--- src/main/java/christmas/exception/DateException.java
+++ src/main/java/christmas/exception/DateException.java
@@ -1,10 +1,19 @@
 package christmas.exception;
 
+import christmas.config.DateConfig;
+
 public class DateException {
     public void numberCheck(String visitDate) {
         try {
             Integer.parseInt(visitDate);
         } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void VisitDateRangeCheckMain(String visitDate) {
+        int date = Integer.parseInt(visitDate);
+        if (date < DateConfig.MONTH_START || date > DateConfig.MONTH_END) {
             throw new IllegalArgumentException();
         }
     }
