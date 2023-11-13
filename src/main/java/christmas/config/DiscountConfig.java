0bd7b34a13ff0f0030602263b03938ca599566a0
jiu6525 <jiu6525@daum.net>
2023-11-13 20:34:25 +0900

    feat : 메뉴와 할인금액의 정보를 담고있는 DiscountConfig 추가

---
+++ src/main/java/christmas/config/DiscountConfig.java
@@ -0,0 +1,10 @@
+package christmas.config;
+
+public class DiscountConfig {
+    public static final int DEFAULT_AMOUNT = 0;
+    public static final int WEEKLY_DISCOUNT_AMOUNT = 2023;
+    public static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
+    public static final int MAIN_EVENT_DISCOUNT_RATE = 100;
+    public static final int MINIMUM_PURCHASE_AMOUNT_FOR_DISCOUNT_EVENT = 10000;
+    public static final int MAXIMUM_ORDER_QUANTITY = 20;
+}
