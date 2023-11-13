6817c6e96872ed01c0bb853c32115edccdec585f
jiu6525 <jiu6525@daum.net>
2023-11-13 20:39:14 +0900

    feat : 주문 수량이 20개 이하 확인하는 메서드 추가

--- src/main/java/christmas/exception/orderException/TotalOrderException.java
+++ src/main/java/christmas/exception/orderException/TotalOrderException.java
@@ -1,5 +1,7 @@
 package christmas.exception.orderException;
 
+import static christmas.config.DiscountConfig.MAXIMUM_ORDER_QUANTITY;
+
 import christmas.domain.Order;
 
 public class TotalOrderException {
@@ -14,4 +16,15 @@ public class TotalOrderException {
     private boolean orderDuplicateCheckSub(String[] userOrders) {
         return new Order(userOrders).getUserOrders().size() == userOrders.length;
     }
+
+    private void orderMenuQuantityCheckMain(String[] orders) {
+        if (orderMenuQuantityCheckSub(orders) > MAXIMUM_ORDER_QUANTITY) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int orderMenuQuantityCheckSub(String[] orders) {
+        return new Order(orders).calculateOrderQuantity();
+    }
+
 }
