465b623cdbec237b8816826dc10e29758ecd0856
jiu6525 <jiu6525@daum.net>
2023-11-13 19:37:53 +0900

    feat : 사용자 입력 메서드 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,20 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String PROMPT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!) ";
+
+    public int userInputVisitDate() {
+        System.out.println(WELCOME_MESSAGE);
+        System.out.println(PROMPT_MESSAGE);
+
+        String visitDate;
+        do {
+            visitDate = Console.readLine().trim();
+        } while (false);
+
+        return Integer.parseInt(visitDate);
+    }
 }
