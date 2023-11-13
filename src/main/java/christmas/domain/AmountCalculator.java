949a1bbf69b611b3df2b594d1b3b4bd1b814a7bb
jiu6525 <jiu6525@daum.net>
2023-11-13 21:24:32 +0900

    feat : 총 혜택을 계산하는 메서드 기능 추가

--- src/main/java/christmas/domain/AmountCalculator.java
+++ src/main/java/christmas/domain/AmountCalculator.java
@@ -4,6 +4,11 @@ import christmas.enums.Prize;
 
 public record AmountCalculator(Amount amount) {
 
+    public int getTotalDiscountAmount() {
+        return amount.mainEventDiscountAmount() + amount.dateDiscountAmount() + amount.specialDateDiscountAmount()
+                + getGiftDiscountAmount();
+    }
+    
     public int getGiftDiscountAmount() {
         if (amount.totalOrderAmount() > Prize.PRODUCT.getMinProductPrice()) {
             return Prize.PRODUCT.getPrice();
