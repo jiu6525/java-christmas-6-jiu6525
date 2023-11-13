13f76c0efae9f3078a4c556a0f9a7aecd6d4b556
jiu6525 <jiu6525@daum.net>
2023-11-13 22:50:39 +0900

    refactor : 출력상수 분리

---
+++ src/main/java/christmas/view/OutputConstants.java
@@ -0,0 +1,19 @@
+package christmas.view;
+
+public class OutputConstants {
+
+    public static final String EVENT_PREVIEW_MESSAGE_FORMAT = "%d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n";
+    public static final String ORDER_MENU_HEADER = "<주문 메뉴>";
+    public static final String ORDER_MENU_BODY = "%s %d개\n";
+    public static final String TOTAL_ORDER_AMOUNT_MESSAGE_FORMAT = "\n<할인 전 총주문 금액>\n%s원\n";
+    public static final String GIFT_MENU_HEADER = "\n<증정 메뉴>";
+    public static final String DISCOUNT_DETAILS_HEADER = "\n<혜택 내역>";
+    public static final String TOTAL_DISCOUNT_AMOUNT_MESSAGE_FORMAT = "\n<총혜택 금액>\n%s원\n";
+    public static final String PAYMENT_AMOUNT_MESSAGE_FORMAT = "\n<할인 후 예상 결제 금액>\n%s원\n";
+    public static final String EVENT_BADGE_MESSAGE_FORMAT = "\n<12월 이벤트 배지>\n%s\n";
+    public static final String CHRISTMAS_DISCOUNT_LABEL = "크리스마스 디데이 할인";
+    public static final String WEEKDAY_DISCOUNT_LABEL = "평일 할인";
+    public static final String SPECIAL_DATE_DISCOUNT_LABEL = "특별 할인";
+    public static final String GIFT_EVENT_LABEL = "증정 이벤트";
+    public static final String NO_DISCOUNT_MESSAGE = "없음";
+}
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,22 +8,6 @@ import java.util.Map;
 
 public class OutputView {
 
-    public static final String EVENT_PREVIEW_MESSAGE_FORMAT = "%d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n";
-    public static final String ORDER_MENU_HEADER = "<주문 메뉴>";
-    public static final String TOTAL_ORDER_AMOUNT_MESSAGE_FORMAT = "\n<할인 전 총주문 금액>\n%s원\n";
-    public static final String GIFT_MENU_HEADER = "\n<증정 메뉴>";
-    public static final String DISCOUNT_DETAILS_HEADER = "\n<혜택 내역>";
-    public static final String TOTAL_DISCOUNT_AMOUNT_MESSAGE_FORMAT = "\n<총혜택 금액>\n%s원\n";
-    public static final String PAYMENT_AMOUNT_MESSAGE_FORMAT = "\n<할인 후 예상 결제 금액>\n%s원\n";
-    public static final String EVENT_BADGE_MESSAGE_FORMAT = "\n<12월 이벤트 배지>\n%s\n";
-    public static final String CHRISTMAS_DISCOUNT_LABEL = "크리스마스 디데이 할인";
-    public static final String WEEKDAY_DISCOUNT_LABEL = "평일 할인";
-    public static final String SPECIAL_DATE_DISCOUNT_LABEL = "특별 할인";
-    public static final String GIFT_EVENT_LABEL = "증정 이벤트";
-    public static final String NO_DISCOUNT_MESSAGE = "없음";
-    public static final String ORDER_MENU_BODY = "%s %d개\n";
-
-
     private final NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.KOREA);
 
     public void orderResultPrint(int visitDate, Order userOrders, AmountCalculator amountCalculator) {
@@ -38,41 +22,42 @@ public class OutputView {
     }
 
     private void printEventPreview(int visitDate) {
-        System.out.printf(EVENT_PREVIEW_MESSAGE_FORMAT, visitDate);
+        System.out.printf(OutputConstants.EVENT_PREVIEW_MESSAGE_FORMAT, visitDate);
     }
 
     private void printOrderMenu(Map<String, Integer> userOrders) {
-        System.out.println(ORDER_MENU_HEADER);
+        System.out.println(OutputConstants.ORDER_MENU_HEADER);
         userOrders.forEach(
-                (menuName, quantity) -> System.out.printf(ORDER_MENU_BODY, menuName, quantity));
+                (menuName, quantity) -> System.out.printf(OutputConstants.ORDER_MENU_BODY, menuName, quantity));
     }
 
     private void printTotalOrderAmount(int totalOrderAmount) {
-        System.out.printf(TOTAL_ORDER_AMOUNT_MESSAGE_FORMAT, numberFormat.format(totalOrderAmount));
+        System.out.printf(OutputConstants.TOTAL_ORDER_AMOUNT_MESSAGE_FORMAT, numberFormat.format(totalOrderAmount));
     }
 
     private void printGiftMenu(String giftEvent) {
-        System.out.println(GIFT_MENU_HEADER);
+        System.out.println(OutputConstants.GIFT_MENU_HEADER);
         System.out.println(giftEvent);
     }
 
     private void printDiscountDetails(AmountCalculator amountCalculator) {
-        System.out.println(DISCOUNT_DETAILS_HEADER);
+        System.out.println(OutputConstants.DISCOUNT_DETAILS_HEADER);
         displayDiscountDetails(amountCalculator);
     }
 
     private void printTotalDiscountAmount(int totalDiscountAmount) {
-        System.out.printf(TOTAL_DISCOUNT_AMOUNT_MESSAGE_FORMAT,
+        System.out.printf(OutputConstants.TOTAL_DISCOUNT_AMOUNT_MESSAGE_FORMAT,
                 numberFormat.format(-totalDiscountAmount));
     }
 
     private void printPaymentAmount(int paymentAmount) {
-        System.out.printf(PAYMENT_AMOUNT_MESSAGE_FORMAT, numberFormat.format(paymentAmount));
+        System.out.printf(OutputConstants.PAYMENT_AMOUNT_MESSAGE_FORMAT, numberFormat.format(paymentAmount));
     }
 
     private void printEventBadge(String badgeEvent) {
-        System.out.printf(EVENT_BADGE_MESSAGE_FORMAT, badgeEvent);
+        System.out.printf(OutputConstants.EVENT_BADGE_MESSAGE_FORMAT, badgeEvent);
     }
+
 
     private void displayDiscountDetails(AmountCalculator amountCalculator) {
         StringBuilder message = new StringBuilder();
@@ -80,17 +65,17 @@ public class OutputView {
         board.forEach((label, amount) -> appendDiscount(message, label, amount));
 
         if (message.length() == 0) {
-            message.append(NO_DISCOUNT_MESSAGE);
+            message.append(OutputConstants.NO_DISCOUNT_MESSAGE);
         }
         System.out.println(message);
     }
 
     private static Map<String, Integer> discountAmountBoard(AmountCalculator amountCalculator) {
         return Map.of(
-                CHRISTMAS_DISCOUNT_LABEL, amountCalculator.amount().mainEventDiscountAmount(),
-                WEEKDAY_DISCOUNT_LABEL, amountCalculator.amount().dateDiscountAmount(),
-                SPECIAL_DATE_DISCOUNT_LABEL, amountCalculator.amount().specialDateDiscountAmount(),
-                GIFT_EVENT_LABEL, amountCalculator.getGiftDiscountAmount()
+                OutputConstants.CHRISTMAS_DISCOUNT_LABEL, amountCalculator.amount().mainEventDiscountAmount(),
+                OutputConstants.WEEKDAY_DISCOUNT_LABEL, amountCalculator.amount().dateDiscountAmount(),
+                OutputConstants.SPECIAL_DATE_DISCOUNT_LABEL, amountCalculator.amount().specialDateDiscountAmount(),
+                OutputConstants.GIFT_EVENT_LABEL, amountCalculator.getGiftDiscountAmount()
         );
     }
 
